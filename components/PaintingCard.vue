247b908cda424b9c62682917928c9aaf9bf4774f
clehr <christian_lehr@outlook.de>
2020-06-30 18:37:02 +0200

    change size of image

--- components/PaintingCard.vue
+++ components/PaintingCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card is-one-quarter">
+  <div class="card column is-one-quarter">
     <div class="card-image">
       <figure class="image">
         <img src="~/assets/sarah.jpeg" alt="Placeholder image" />
@@ -7,18 +7,11 @@
     </div>
     <div class="card-content">
       <div class="media">
-        <div class="media-left">
-          <figure class="image is-48x48">
-            <img src="~/assets/sarah.jpeg" alt="Placeholder image" />
-          </figure>
-        </div>
         <div class="media-content">
-          <p class="title is-4">Sarah Navarrete Gaitan</p>
-          <p class="subtitle is-6">Email: sarah.lehr92@gmx.de</p>
+          <p class="title is-4">Ölgemälde 600x300</p>
+          <p class="subtitle is-6">Preis: 30$</p>
         </div>
       </div>
-
-      <div class="content">Hey my name is Sarah and I'm a freshbaked tattoo artist.</div>
     </div>
   </div>
 </template>
